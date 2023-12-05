7a747c8a404e9fdf56a0b196df02e299ac7430b7
user <sk87732925@gmail.com>
2023-12-05 21:45:32 +0900

    fix

--- src/entity/UserInfo.java
+++ src/entity/UserInfo.java
@@ -2,10 +2,10 @@ package entity;
 
 public class UserInfo {
 
-    private String Id;
+    private static String Id;
     private int coin;
     private double remained_lives;
-    private int highest_score;
+    private static int highest_score;
 
     public UserInfo (String Id, int coin, double remained_lives, int highest_score) {
         this.Id = Id;
@@ -13,7 +13,7 @@ public class UserInfo {
         this.remained_lives = remained_lives;
         this.highest_score = highest_score;
     }
-    public String getId() {
+    public static String getId() {
         return Id;
     }
     public int getcoin() {
@@ -22,7 +22,7 @@ public class UserInfo {
     public double getRemained_lives() {
         return remained_lives;
     }
-    public int getHighest_score() {
+    public static int getHighest_score() {
         return highest_score;
     }
 
